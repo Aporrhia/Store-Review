6ad3bb8f58f7ba609d0fe8c3e515a2d15e798971
Aporrhia <top4ik1899@gmail.com>
2025-09-10 10:41:05 +0300

    TK-16 | Create seeder for Products

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Order extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     protected $table = 'orders';
 
